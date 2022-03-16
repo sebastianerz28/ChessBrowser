197a8c71410e6768423804012560140aaca2fb74
sebastianerz28 <sebastianerz@gmail.com>
2022-03-15 21:51:26 -0600

    Init Commit

---
+++ ChessTools/ChessGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ChessTools
+{
+
+    public class ChessGame
+    {
+
+        //Internal CTR
+        public string Event { get;  set; } = "0";
+        public string Site { get;  set; } = "1";
+        public string Round { get;  set; } = "2";
+        public string White { get;  set; } = "3";
+        public string Black { get;  set; } = "4";
+        public string WhiteElo { get;  set; } = "5";
+        public string BlackElo { get; set; } = "6";
+        public string Result { get;  set; } = "7";
+        public string EventDate { get;  set; } = "8";
+        public string Moves { get;  set; } = "9";
+
+        
+        private string[] data;
+        public ChessGame() { data = new string[10]; }
+
+        public void setVal(int index, string line)
+        {
+            data[index] = line;
+        }
+
+        public bool ValsNull()
+        {
+            foreach(string line in data)
+            {
+                if (line == null)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ChessTools/PGNReader.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace ChessTools
+{
+    class PGNReader
+    {
+        public List<ChessGame> games;
+
+        PGNReader()
+        {
+            games = new List<ChessGame>();
+
+        }
+
+        /*
+         * 
+    The tagged data for a game [...] always comes before the moves.
+    After all the tagged data is a blank line, followed by the moves. The moves can contain newlines.
+    The end of the moves (and game) is indicated by another blank line.
+    All games will have the required tags listed above, but they may not be in any particular order.
+    The values inside tags can not contain [ or ] characters, but they can contain other special characters, such as " or \.
+    The name of the tag will always directly follow the '[' character, followed by a space, followed by the value value which is always enclosed in quotes.
+
+         */
+        /// <summary>
+        /// Reads a line makes a chess game object and adds it to the lists
+        /// </summary>
+        /// <param name="filename"></param>
+        public void ReadLines(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            ChessGame game = new ChessGame();
+            foreach (string line in lines)
+            {
+
+
+                string temp = "";
+                if (line[0] == '[')
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        int leftBCount = 0;
+                        int rightBCount = 0;
+                        //Check brackets
+                        if (line[i] == '[')
+                        {
+                            leftBCount++;
+                        }
+                        else if (line[i] == ']')
+                        {
+                            rightBCount++;
+                        }
+                        if (line[i] == '"')
+                        {
+                            while (line[i] != ']')
+                            {
+                                temp += line[i];
+                                i++;
+                            }
+                            if (line[line.Length - 1] != ']')
+                            {
+                                throw new Exception("No Closing bracket");
+                            }
+                            if (line[line.Length - 2] != '"')
+                            {
+                                throw new Exception("There are no closing quotes");
+                            }
+                        }
+
+                        if (leftBCount != rightBCount && leftBCount > 1 || rightBCount > 1)
+                        {
+                            throw new Exception("Brackets Not allowed");
+                        }
+
+                    }
+                }
+                else if (line == "")
+                    continue;
+                else
+                {
+                    temp += line;
+                }
+
+                if (game.ValsNull())
+                {
+
+                }
+
+            }
+        }
+
+        
+
+    }
+}
